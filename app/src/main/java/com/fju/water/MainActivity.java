409ba21b855b5e16d0355e9b4bf795279d17de06
miia <miia@com.tw>
2019-11-19 16:28:37 +0800

    4

--- app/src/main/java/com/fju/water/MainActivity.java
+++ app/src/main/java/com/fju/water/MainActivity.java
@@ -10,78 +10,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-   /* @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                edmonth = findViewById(R.id.month);
-                ednext = findViewById(R.id.next);
-                Button button = findViewById(R.id.button);
-                button.setOnClickListener(new View.OnClickListener(){
-            }
-        });
-    }
-    private EditText edmonth ;
-    private EditText ednext ;
-
-    public void onClick(View view){
-        String monthString = edmonth.getText().toString();
-        if(!TextUtils.isEmpty(monthString)){
-            float degree = Float.parseFloat(monthString);
-            float onClick = 0;
-            if(degree>=1&&degree<=10){
-                onClick = degree*7.35f;
-            }else if(degree>=11 && degree<=30){
-                onClick = degree*9.45f-21;
-
-            }else if(degree>=31&&degree<=50){
-                onClick = degree*11.55f-84;
-            }else{
-                onClick = degree*12.075f-110.5f;
-            }
-
-    }else{
-            String nextString = ednext.getText().toString();
-            if(!TextUtils.isEmpty(nextString)){
-                float degree = Float.parseFloat(nextString);
-                float onClick = 0;
-                if(degree>=1&&degree<=10){
-                    onClick = degree*7.35f;
-                }else if(degree>=11 && degree<=30){
-                    onClick = degree*9.45f-21;
-
-                }else if(degree>=31&&degree<=50){
-                    onClick = degree*11.55f-84;
-                }else{
-                    onClick = degree*12.075f-110.5f;
-                }
-            }
-        }
-        }
-
-
-
-}
-*/
-   private EditText edNext;
+     private EditText edNext;
     private EditText edMonthly;
 
     @Override
@@ -90,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/fju/water/resultActivity.java
+++ app/src/main/java/com/fju/water/resultActivity.java
@@ -1,5 +1,6 @@
 package com.fju.water;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,25 +9,30 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class resultActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_result);
+            Intent intent = getIntent();
+            float fee = intent.getFloatExtra("FEE",-1);
+            Log.d("ResultActivity",fee+"");
+            TextView feeText = findViewById(R.id.fee) ;
+            int n = (int) (fee+0.5f);
+            feeText.setText(n+"");
+
+        }
+
+
     }
 
-}
+
+
+
